dd2f927b097545b5b35db61b7ae4d668e6384d09
steve <steve@arca.leo>
2019-11-23 16:37:05 -0500

    Clean up setup.py code to comply with Python coding standard Updated download_url.  (feels weird to update version after this) Added 'install_requires'.

--- setup.py
+++ setup.py
@@ -10,50 +10,50 @@ except ImportError:
 import io
 
 # The text of the README file
-README_name = __file__.replace("setup.py", "README.md")
-with io.open(README_name, encoding="utf8") as README:
+README_name = __file__.replace('setup.py', 'README.md')
+with io.open(README_name, encoding='utf8') as README:
     bind9_parser_main_doc = README.read()
 
 packages = [
-    "bind9_parser",
+    'bind9_parser',
 ]
 
-bind9_parser_version = "0.9.8"
+bind9_parser_version = '0.9.8'
 
 setup(  # Distribution meta-data
-    name="bind9_parser",
+    name='bind9_parser',
     version=bind9_parser_version,
-    description="Bind9 configuration file parsing module",
+    description='Bind9 configuration file parsing module',
     long_description=bind9_parser_main_doc,
-    long_description_content_type="text/x-md",
-    author="Steve Egbert",
-    author_email="egberts@yahoo.com",
+    long_description_content_type='text/x-md',
+    author='Steve Egbert',
+    author_email='egberts@yahoo.com',
     keywords='bind9 configuration parser isc named.conf',
-    url="https://github.com/egberts/bind9_parser/",
-    download_url="https://pypi.org/project/bind9_parser/",
-    license="MIT License",
+    url='https://github.com/egberts/bind9_parser/',
+    download_url='https://github.com/egberts/bind9-parser/archive/0.9.8.tar.gz',
+    license='MIT License',
     packages=packages,
-    python_requires=">=3.5",
-    setup_requires=["pytest-runner"],
-    tests_require=["pytest"],
-#    py_modules=['bind9_parser'],
+    python_requires='>=3.5',
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    install_requires=['pyparsing'],
     platforms='any',
     classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Developers",
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
-        "Intended Audience :: Information Technology",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: Implementation :: PyPy",
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Security',
         'Topic :: Software Development :: Libraries',
         'Topic :: System :: Logging',
