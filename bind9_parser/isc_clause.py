1526b935ce0dd29c9826c7366c2e7e395dea6145
steve <steve@arca.leo>
2019-11-23 15:58:14 -0500

    had to rework group of zone clauses so that it would make a list (and not a dict) of zone clauses.

--- bind9_parser/isc_clause.py
+++ bind9_parser/isc_clause.py
@@ -9,8 +9,7 @@ Title: Clause Statement for all clauses
 Description: Provides ALL clauses at the top-level of ISC
              configuration file (isc_clause_*.py)
 """
-from pyparsing import ZeroOrMore, cppStyleComment, pythonStyleComment, Group
-from bind9_parser.isc_utils import parse_me
+from pyparsing import ZeroOrMore, OneOrMore, Group, ungroup
 from bind9_parser.isc_clause_acl import clause_stmt_acl_standalone
 from bind9_parser.isc_clause_controls import clause_stmt_control_standalone
 from bind9_parser.isc_clause_dlz import clause_stmt_dlz_standalone
@@ -42,11 +41,11 @@ optional_clause_stmt_set = (
         | clause_stmt_server_standalone
         | clause_stmt_trusted_keys_standalone
         | clause_stmt_view_standalone
-        | clause_stmt_zone_standalone
+        | clause_stmt_zone_standalone('zones25')
 )
 
 optional_clause_stmt_series = (
-    ZeroOrMore(
+    OneOrMore(
         optional_clause_stmt_set
     )
 )
@@ -54,14 +53,21 @@ optional_clause_stmt_series = (
 
 # Exactly one 'options' clause
 # options { a; };
-mandatory_clause_stmts = clause_stmt_options
+mandatory_clause_stmt_set = clause_stmt_options
 
 # Use the ZeroOrMore(optional) & mandatory & ZeroOrMore(optional) approach
-clause_statements = (
-        ZeroOrMore(optional_clause_stmt_set)
-        - mandatory_clause_stmts
-        + ZeroOrMore(optional_clause_stmt_set)
+# clause_statements = (
+#         ZeroOrMore(optional_clause_stmt_set)
+#         - OneOrMore(mandatory_clause_stmts)
+#         + ZeroOrMore(optional_clause_stmt_set)
+# )
+# clause_statements.setName('(clauses-statements)')
+# clause_statements.ignore(cppStyleComment)
+# clause_statements.ignore(pythonStyleComment)
+
+# TODO: Unable to enforce mixed mode 1-* and 1-1 clauses (external logic required here?)
+# TODO: BUG https://github.com/pyparsing/pyparsing/issues/167
+clause_statements = ZeroOrMore(
+    optional_clause_stmt_set
+    | mandatory_clause_stmt_set
 )
-clause_statements.setName('(clauses-statements)')
-clause_statements.ignore(cppStyleComment)
-clause_statements.ignore(pythonStyleComment)
